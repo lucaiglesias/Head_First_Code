fb8087dd1d39b48624ab09ca11b0862b6d00529b
Luca Iglesias <lucaiglesias@gmail.com>
2018-05-23 10:09:14 -0300

    Adicionado Pasta Chapter 2

---
+++ Chapter 2/dogcalc.py
@@ -0,0 +1,4 @@
+dog_name = input("Type yours dog name: ")
+dog_age = input("Enter dog's age: ")
+human_age = int(dog_age)*7
+print('Your dog',dog_name,'is',human_age,'years old in human years')
---
+++ Chapter 2/swap.py
@@ -0,0 +1,7 @@
+first = 'somewhere'
+last = 'over the rainbow'
+print(first, last)
+swap = last
+last = first
+first = swap
+print(first, last)
