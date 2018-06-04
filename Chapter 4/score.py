f96e41214cdf928fb75b87460d5080374f13cc62
Luca Iglesias <lucaiglesias@gmail.com>
2018-06-04 17:36:39 -0300

    Update Score

--- Chapter 4/score.py
+++ Chapter 4/score.py
@@ -1,19 +1,19 @@
 import random
 end = ''
-solutionnum = " "
 
 #first we need to creat a array (or in python languagem 'list)
 scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54]
 
 #then, we will use the loop function to keep the code running
 while end != 'n':
-    
+#Enter the value of solution
     solution = input("Type number of solution ")
     solutionnum = int(solution)
+    #If solution do not exist enter in a loop
     while (solutionnum < 0 or solutionnum > 9 ):
         solution = input("Type number of solution ")
         solutionnum = int(solution)
-
+#pick value and look position in array, then print the value in the array
     score = scores[solutionnum]
     print ("Solution #", solutionnum, "produce", score, "bubbles.")
     end = input ("Type another? y/n ")
