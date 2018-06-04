402788a5da54d8151ff6ae7279b1f27baf2e0d17
Luca Iglesias <lucaiglesias@gmail.com>
2018-06-04 19:05:05 -0300

    Add thing-a-ma-jig.py in Chapter 4

---
+++ Chapter 4/thing-a-ma-jig.py
@@ -0,0 +1,19 @@
+#characters = ['t', 'a', 'c', 'o']
+#characters = ['a', 'm', 'a', 'n', 'a', 'p', 'l', 'a', 'n', 'a', 'c']
+characters = [ 'w', 'a', 's', 'i', 't', 'a', 'r']
+
+output = ''
+length = len(characters)
+i = 0
+while (i < length):
+    output = output + characters[i]
+    i = i + 1
+
+length = length * -1
+i = -2
+
+while (i >= length):
+    output = output + characters[i]
+    i = i - 1 
+
+print (output)
