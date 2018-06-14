0036d8c2f18f2511f8a5088fe66c78824431be08
Luca Iglesias <lucaiglesias@gmail.com>
2018-06-14 19:03:51 -0300

    Add Smoothies.py

---
+++ Chapter 4/smoothies.py
@@ -0,0 +1,8 @@
+#Test "for"
+
+# creating a array
+smoothies = ['coconut', 'strawberry', 'banana', 'pinaple', 'acai berry']
+
+for smoothie in smoothies:
+    output = 'We serve ' + smoothie
+    print(output)
