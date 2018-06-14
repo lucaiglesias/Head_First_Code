85b35141c2d5b38bb72babd1acfaad94f5eed9e8
Luca Iglesias <lucaiglesias@gmail.com>
2018-06-14 19:13:27 -0300

    Update Smoothies.py

--- Chapter 4/smoothies.py
+++ Chapter 4/smoothies.py
@@ -3,6 +3,11 @@
 # creating a array
 smoothies = ['coconut', 'strawberry', 'banana', 'pinaple', 'acai berry']
 
+length = len(smoothies)
+
 for smoothie in smoothies:
     output = 'We serve ' + smoothie
     print(output)
+
+for i in range (length):
+    print('Smoothie #', i, smoothies[i])
